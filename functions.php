1b7b85239a3b2dc25dff8b686ee286b19614ec1e
Omar ElHawary <omaraelhawary@gmail.com>
2025-01-04 18:38:17 +0200

    Convert Header to new Modern Block

--- functions.php
+++ functions.php
@@ -297,12 +297,13 @@ class PlaceholderBlock {
 
 function ourNewBlocks(){
     register_block_type_from_metadata( __DIR__ . '/build/footer' );
+    register_block_type_from_metadata( __DIR__ . '/build/header' );
 }
 
 add_action('init', 'ourNewBlocks');
 
 new PlaceholderBlock('eventsandblogs');
-new PlaceholderBlock('header');
+//new PlaceholderBlock('header');
 //new PlaceholderBlock('footer');
 new PlaceholderBlock('singlepost');
 new PlaceholderBlock('page');
